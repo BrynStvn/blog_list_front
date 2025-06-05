6c32dbcb47d88eea64c1e706e0baf5abe8fab298
BrynStvn <bry.stvn98@gmail.com>
2025-06-05 10:13:51 -0500

    Autenticación con usuario

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,46 @@
-const Login = ({ handleInputChange }) => {
+import Notification from "./Notification"
+import PropTypes from 'prop-types'
+
+const LoginForm = (props) => {
     return (
-        <>
         <div>
-            <label>username </label>
-            <input
-                type="text"
-                onChange={handleInputChange}
-                placeholder=""
-            />
+            <h2>Login</h2>
+            <Notification message={props.message}/>
+            <form onSubmit={props.handleLogin}>
+                <div>
+                    <label htmlFor="username">username </label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={props.username}
+                        name="Username"
+                        onChange={({ target }) => props.setUsername(target.value)}
+                        autoComplete="username"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">password </label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={props.password}
+                        name="Password"
+                        onChange={({ target }) => props.setPassword(target.value)}
+                        autoComplete="current-password"
+                    />
+                </div>
+                <button type="submit">login</button>
+            </form>
         </div>
-        <div>
-            <label>password </label>
-            <input
-                type="password"
-                /* onChange={handleInputChange} */
-                placeholder=""
-            />
-        </div>
-        </>
     )
 }
 
-export default Login
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
+
+export default LoginForm
